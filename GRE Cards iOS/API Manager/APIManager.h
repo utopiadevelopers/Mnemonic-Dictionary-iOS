5fe1f2e15c5367cc460d8398b1942ebc02fc4c5c
Satyam Krishna <satyamkrishna92@gmail.com>
2014-11-08 19:43:06 +0530

    Api Manager

---
+++ GRE Cards iOS/API Manager/APIManager.h
@@ -0,0 +1,13 @@
+//
+//  APIManager.h
+//  GRE Cards iOS
+//
+//  Created by Satyam Krishna on 07/11/14.
+//  Copyright (c) 2014 Utopia Developers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIManager : NSObject
+
+@end
---
+++ GRE Cards iOS/API Manager/APIManager.m
@@ -0,0 +1,13 @@
+//
+//  APIManager.m
+//  GRE Cards iOS
+//
+//  Created by Satyam Krishna on 07/11/14.
+//  Copyright (c) 2014 Utopia Developers. All rights reserved.
+//
+
+#import "APIManager.h"
+
+@implementation APIManager
+
+@end
